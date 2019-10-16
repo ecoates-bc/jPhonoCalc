2eedf023ba48a998a8dcb886acbeb7e2cfc3a490
edith <ecoates.bc@gmail.com>
2019-10-16 09:46:51 -0700

    Created ScannerTools class, added try/catch for file upload

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -4,26 +4,17 @@ import model.CorpusReader;
 import phonology.Language;
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Scanner input;
         Language english = new Language();
         CorpusReader reader = new CorpusReader(english, "data/english.txt");
-        input = new Scanner(System.in);
-        String path = "";
+        ScannerTools tools = new ScannerTools();
 
-        System.out.println("Input file path:");
-        path = "data/" + input.nextLine();
-        reader.read(path);
-        reader.analyze();
-        System.out.println(reader.language.inventory.get(4).getOppositeBefores());
+        tools.handleUpload(reader);
 
-        System.out.println("Name output file:");
-        path = input.nextLine();
-        reader.save(path);
+        tools.handleSave(reader);
         System.out.println("End of program");
     }
 }
---
+++ src/main/ui/ScannerTools.java
@@ -0,0 +1,36 @@
+package ui;
+
+import model.CorpusReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class ScannerTools {
+    public Scanner input;
+    private String path;
+
+    public ScannerTools() {
+        input = new Scanner(System.in);
+        path = "";
+    }
+
+    public void handleUpload(CorpusReader reader) {
+        System.out.println("Input file path:");
+        path = "data/" + input.nextLine();
+        try {
+            reader.read(path);
+        } catch (IOException e) {
+            System.out.println("File not found. Try again.");
+            handleUpload(reader);
+        }
+        reader.analyze();
+    }
+
+    public void handleSave(CorpusReader reader) throws FileNotFoundException, UnsupportedEncodingException {
+        System.out.println("Name output file:");
+        path = input.nextLine();
+        reader.save(path);
+    }
+}
